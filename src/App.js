514a2dc8236e6f1c96c6b97f09a848e6c570648a
Range Quest <range.quest@gmail.com>
2021-09-16 13:47:06 +0530

    Config module and httpService module implemented.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import React, { Component } from "react";
-import axios from "axios";
+import http from "./services/httpService";
 import "./App.css";
-
-axios.interceptors.response.use(null, error => {
-  const expectedError = error.response && error.response.status >= 400 && error.response.status < 500;
-
-  if (!expectedError) {
-    console.log("Logging the error", error);
-    alert("An Unexpected Error Occured!");
-  }
-
-  return Promise.reject(error);
-});
-
-const apiEndpoint = "https://jsonplaceholder.typicode.com/posts";
+import config from "./config.json";
 
 class App extends Component {
   state = {
@@ -21,20 +9,20 @@ class App extends Component {
   };
 
   componentDidMount = async () => {
-    const { data: posts } = await axios.get(apiEndpoint);
+    const { data: posts } = await http.get(config.apiEndpoint);
     this.setState({ posts });
   };
 
   handleAdd = async () => {
     const obj = { title: "a", body: "b" };
-    const { data: post } = await axios.post(apiEndpoint, obj);
+    const { data: post } = await http.post(config.apiEndpoint, obj);
     const posts = [post, ...this.state.posts];
     this.setState({ posts });
   };
 
   handleUpdate = async post => {
     post.title = "Updated";
-    const { data: updatedPost } = await axios.put(apiEndpoint + "/" + post.id, post);
+    const { data: updatedPost } = await http.put(config.apiEndpoint + "/" + post.id, post);
     const posts = [...this.state.posts];
     const index = posts.indexOf(post);
     posts[index] = { ...updatedPost };
@@ -48,7 +36,7 @@ class App extends Component {
     this.setState({ posts });
 
     try {
-      await axios.delete(apiEndpoint + "/" + post.id);
+      await http.delete(config.apiEndpoint + "/" + post.id);
     } catch (ex) {
       if (ex.response && ex.response.status === 404) alert("This post has already been deleted.");
       this.setState({ posts: originalPosts });
