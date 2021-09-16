77e2850b676356490c7211327335e59d846eb6aa
Range Quest <range.quest@gmail.com>
2021-09-16 13:05:49 +0530

    Optimistic updates with references to the original states.

--- src/App.js
+++ src/App.js
@@ -31,9 +31,18 @@ class App extends Component {
   };
 
   handleDelete = async post => {
-    await axios.delete(apiEndpoint + "/" + post.id);
+    const originalPosts = this.state.posts;
+
     const posts = this.state.posts.filter(p => p.id !== post.id);
     this.setState({ posts });
+
+    try {
+      await axios.delete(apiEndpoint + "/" + post.id);
+      throw new Error("this is delete error!");
+    } catch (ex) {
+      alert("Something failed while deleting a post!");
+      this.setState({ posts: originalPosts });
+    }
   };
 
   render() {
