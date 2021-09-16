839d36b19bf0601672480545db7cb015d75f0fa4
Range Quest <range.quest@gmail.com>
2021-09-16 14:54:13 +0530

    Extracted the Sentry logger to logger module.

--- src/index.js
+++ src/index.js
@@ -4,18 +4,9 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
-import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
+import logger from "./services/logService";
 
-Sentry.init({
-  dsn: "https://4885944b90614ae9826de550b6bd3ce2@o1002268.ingest.sentry.io/5962420",
-  integrations: [new Integrations.BrowserTracing()],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
+logger.init();
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 import { toast } from "react-toastify";
-import * as Sentry from "@sentry/react";
+import logger from "./logService";
 
 axios.interceptors.response.use(null, error => {
   const expectedError = error.response && error.response.status >= 400 && error.response.status < 500;
 
   if (!expectedError) {
-    Sentry.captureException(error);
+    logger.log(error);
     toast.error("An Unexpected Error Occured!");
   }
 
